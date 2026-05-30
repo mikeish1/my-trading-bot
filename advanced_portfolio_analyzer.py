cb5cc9743241c435fbab7ab830e6f386a912a8c5
mikeish1 <83454328+mikeish1@users.noreply.github.com>
2026-05-30 10:29:44 -0600

    Update advanced_portfolio_analyzer.py

--- advanced_portfolio_analyzer.py
+++ advanced_portfolio_analyzer.py
@@ -15,7 +15,7 @@ from polygon import RESTClient
 # Pre-configured client
 import os
 POLYGON_API_KEY = os.environ.get("POLYGON_API_KEY")
-client = RESTClient(api_key=POLYGON_API_KEY)
+client = RESTClient(api_key="DEMO")
 
 def fetch_historical_bars(tickers, days=90, multiplier=1, timespan="day", max_retries=3):
     """
